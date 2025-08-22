f736134596ad3de79edd9ab0096b570a74c34583
Hari Shankar Singh <hari@lmg8.tech>
2025-08-22 08:09:31 +0530

    Add project files.

---
+++ Count the Number of Vowel Strings in Range/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(CountVowelInString(["hey", "aeo", "mu", "ooo", "artro"],1,4)); //output 3
+        Console.WriteLine(CountVowelInString(["are", "amy", "u"], 0, 2)); //output 2
+    }
+    public static int CountVowelInString(string[] words, int left, int right)
+    {
+        int count= 0;
+        for (int i = left; i <= right; i++)
+        {
+            string word = words[i];
+           int lefts = 0;
+            int rights = words[i].Length-1;
+            if (IsVowel(word[lefts]) && IsVowel(word[rights]))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    private static bool IsVowel(char ch)
+    {
+        return ("aeiou").IndexOf(ch) >= 0;
+    }
+}
